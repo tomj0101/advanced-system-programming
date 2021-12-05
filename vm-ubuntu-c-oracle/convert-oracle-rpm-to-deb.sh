37592378d5b965692f963eef366515d9a618b4e3
Tom <tomasjimenezsantos@gmail.com>
2021-12-05 14:01:32 -0400

    Update convert-oracle-rpm-to-deb.sh

--- vm-ubuntu-c-oracle/convert-oracle-rpm-to-deb.sh
+++ vm-ubuntu-c-oracle/convert-oracle-rpm-to-deb.sh
@@ -8,6 +8,8 @@ echo "=== Conversion start ==="
 sudo alien --scripts -d oracle-database-xe-21c-1.0-1.ol8.x86_64.rpm
 echo "=== Conversion finished ==="
 
+sudo chmod 777 oracle-database-xe-21c_1.0-2_amd64.deb
+
 ls -lrt
 date 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
