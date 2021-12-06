b2f492371e9759b3d4db6868cc555fb55ee58c6a
tomj0101 <tomasjimenezsantos@gmail.com>
2021-12-06 01:30:23 +0000

    simple example

--- vm-ubuntu-c-oracle/python_oracle/samplexml.py
+++ vm-ubuntu-c-oracle/python_oracle/samplexml.py
@@ -3,25 +3,18 @@ import xmlschema
 import xml.etree.ElementTree as ET
 
 # to make it simple use external XML Schema and create a local file from it to validate XML examples
-
-xsd_url = "collection.xsd"
-
-with open("test.xsd", "w", newline="") as out:
-    out.write(requests.get(xsd_url).text)
-xsd = xmlschema.XMLSchema("test.xsd")
+xsd = xmlschema.XMLSchema("collection.xsd")
 
 # XML #1 validates to the Schema
-url1 = "collection.xml"
-xt = ET.fromstring(requests.get(url1).text)
-print("xml1 valid=", xsd.is_valid(xt))
+print("xml1 valid=", xsd.is_valid("collection.xml"))
 
-xml1 = ET.parse(url1)
-print(xml1.getroot())
 
 # XML #2 with invalid structure
-url2 = "collection-1_error.xml"
-xt = ET.fromstring(requests.get(url2).text)
-print("xml2 valid=", xsd.is_valid(xt))
+print("xml2 valid=", xsd.is_valid("collection-1_error.xml"))
+
+# print xml
+col1 = open("collection.xml","r")
+print(col1.read())
 
 # pre-setup
 # pip3 install requests
