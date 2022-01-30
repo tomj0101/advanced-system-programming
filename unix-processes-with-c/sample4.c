86f3215de0a4c2841f29463488fd274e44e8237e
tomj0101 <tomasjimenezsantos@gmail.com>
2022-01-30 07:13:29 +0000

    adding some validation

--- unix-processes-with-c/sample4.c
+++ unix-processes-with-c/sample4.c
@@ -1,4 +1,8 @@
-//Pass Information between child process and parent process 
+//Pass Information between child process and parent process
+//In developer machine, active man commands like  'man fork' , run the next command.
+/*
+$ sudo 
+*/
 #include <stdio.h> 
 #include <unistd.h>
 
@@ -12,15 +16,31 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 	int id = fork();
+	if(id == -1){
+		printf("An error ocurred with opening the fork \n");
+		return 4;
+	}
 	if(id == 0){
+		//child process
 		close(fd[0]);
 		int x;
 		printf("Input a number: ");
 		scanf("%d", &x);
-		write(fd[1],&x,sizeof(int));
+		if(write(fd[1],&x,sizeof(int)) == -1){
+			printf("An error ocurred with writing to the pipe \n");
+			return 2;
+		}
 		close(fd[1]);
 	}else{
-	
+		//parent process
+		close(fd[1]);
+		int y;
+		if(read(fd[0],&y,sizeof(int)) == -1){
+			printf("An error ocurred with reading to the pipe \n");
+			return 3;
+		}
+		close(fd[0]);
+		printf("Got from child process: %d \n",y);	
 	}
 	
 	return 0;
