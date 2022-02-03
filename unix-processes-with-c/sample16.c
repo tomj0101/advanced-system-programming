5ffc5d6c8ae2192790eef3ad33c5c16a70ee98cf
tomj0101 <tomasjimenezsantos@gmail.com>
2022-02-03 06:50:25 +0000

    passing string through pipe code fix

--- unix-processes-with-c/sample16.c
+++ unix-processes-with-c/sample16.c
@@ -1,65 +1,90 @@
 //Send string through a pipe
-//Bug, should be fix tomorrow! :)
+//Bug fix it, can't wait until tomorrow
+//
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-
-#include <sys/types.h>
-#include <unistd.h>
-
-#include <sys/wait.h>
 #include <string.h>
-
-int main(int argc, char* argv[]){
-
-	int fd[2];
-	if(pipe(fd) == -1){
-		return 1;
-	}
-
-	int pid = fork();
-	if(pid < 0){
-		return 2;
-	}
-
-	if(pid == 0){
-		//child process
-		close(fd[0]);
-		char str[200];
-		printf("Input string: ");
-		fgets(str, 200, stdin);
-		str[strlen(str) - 1] = '\0';
-		
-		int n = strlen(str) + 1; //the + 1 is for the last charater of string NULL
-
-		//there is no address for strlen() is evalue in the CPU, no in the memory
-		if(write(fd[1], &n, sizeof(int)) < 0){
-			return 4;
-		}
-		//if(write(fd[1], str, sizeof(char) * (strlen(str)+1)) < 0){
-		if(write(fd[1], str, sizeof(char) * n) < 0){
-			return 3;
-		}
-		close(fd[1]);
-	
-	}else{
-		//parent process
-		close(fd[1]);
-		char str[200];
-		int n;
-
-		if(read(fd[0], &n, sizeof(int)) < 0){
-			return 5;
-		}
-		if(read(fd[0], str, sizeof(char) * n)){
-			return 6;
-		}
-		wait(NULL);
-		printf("Received: %s \n",str);
-		close(fd[0]);
-		wait(NULL);
-	
-	
-	}
-	return 0;
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+ 
+int main()
+{
+    // We use two pipes
+    // First pipe to send input string from parent
+    // Second pipe to send concatenated string from child
+ 
+    int fd1[2]; // Used to store two ends of first pipe
+    int fd2[2]; // Used to store two ends of second pipe
+ 
+    char fixed_str[] = " coding for FUN!";
+    char input_str[100];
+    pid_t p;
+ 
+    if (pipe(fd1) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+    if (pipe(fd2) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+ 
+    scanf("%s", input_str);
+    p = fork();
+ 
+    if (p < 0) {
+        fprintf(stderr, "fork Failed");
+        return 1;
+    }
+ 
+    // Parent process
+    else if (p > 0) {
+        char concat_str[100];
+ 
+        close(fd1[0]); // Close reading end of first pipe
+ 
+        // Write input string and close writing end of first
+        // pipe.
+        write(fd1[1], input_str, strlen(input_str) + 1);
+        close(fd1[1]);
+ 
+        // Wait for child to send a string
+        wait(NULL);
+ 
+        close(fd2[1]); // Close writing end of second pipe
+ 
+        // Read string from child, print it and close
+        // reading end.
+        read(fd2[0], concat_str, 100);
+        printf("Concatenated string %s\n", concat_str);
+        close(fd2[0]);
+    }
+ 
+    // child process
+    else {
+        close(fd1[1]); // Close writing end of first pipe
+ 
+        // Read a string using first pipe
+        char concat_str[100];
+        read(fd1[0], concat_str, 100);
+ 
+        // Concatenate a fixed string with it
+        int k = strlen(concat_str);
+        int i;
+        for (i = 0; i < strlen(fixed_str); i++)
+            concat_str[k++] = fixed_str[i];
+ 
+        concat_str[k] = '\0'; // string ends with '\0'
+ 
+        // Close both reading ends
+        close(fd1[0]);
+        close(fd2[0]);
+ 
+        // Write concatenated string and close writing end
+        write(fd2[1], concat_str, strlen(concat_str) + 1);
+        close(fd2[1]);
+ 
+        exit(0);
+    }
 }
