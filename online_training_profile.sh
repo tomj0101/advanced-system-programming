4e2b5a679d09bcbf57b1c80f2e6cbbb004d77b91
Tom <tomasjimenezsantos@gmail.com>
2024-10-08 08:30:37 -0400

    Create online_training_profile.sh

---
+++ online_training_profile.sh
@@ -0,0 +1,17 @@
+# Full Path
+# PS1='\u@\h:\w\$ '
+#\u – the username.
+#\h – the hostname.
+#\w – the current working directory (this is what shows the path).
+#\$ – a $ for normal users or # for root users.
+# example: john@host1:~/Documents/git_repos/perf
+
+
+
+# modified
+#PS1='\u@\h\$ '
+# example developer@devserver1
+
+# modified
+PS1='\$ '
+# $ 
