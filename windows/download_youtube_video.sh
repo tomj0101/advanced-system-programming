f0eaca312b44b6090924d499138f03c2ce5168c8
Tom <tomasjimenezsantos@gmail.com>
2025-08-22 02:06:38 -0400

    Create download_youtube_video.sh

---
+++ windows/download_youtube_video.sh
@@ -0,0 +1,16 @@
+Download youtube Video
+### 1. Install yt-dlp
+```
+pip install yt-dlp
+```
+
+### 2. Download video as MP4
+```
+yt-dlp -f mp4 "https://www.youtube.com/watch?v=VIDEO_ID"
+```
+
+---
+### 1. Download using curl
+```
+curl -L -o video.mp4 "$(yt-dlp -g https://www.youtube.com/watch?v=VIDEO_ID)"
+```
